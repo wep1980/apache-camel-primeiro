107cd3ecb3cff316581a756138df7d2a763abd51
wep1980 <wepbike@gmail.com>
2022-03-08 15:28:30 -0300

    Passando parametros dinamicamente com setProperty()

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -22,21 +22,17 @@ public class RotaPedidos {
 				 * noop=true -> Comando para nao apagar os arquivos da pasta pedidos ao transferir para a pasta saida
 				 */
 				from("file:pedidos?delay=5s&noop=true")
+						.setProperty("pedidoId", xpath("/pedido/id/text()")) // Definindo variavel da rota, pedidoId(setando uma propriedade)
+						.setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")) // Definindo variavel da rota, clienteId(setando uma propriedade)
 						.split()// Dividindo o conteudo do arquivo por item
 						   .xpath("/pedido/itens/item")
-						   //.log("${body}") // Mostrando o ID e o corpo de cada mensagem atraves do log
 						.filter()
-						   //.xpath("/pedido/itens/item/formato[text()='EBOOK']") // Acessando um elemento dentro do arquivo XML, para isso e inserido o caminho ate chegar no elemento formato, e trazer apenas os elementos que sejam do formato EBOOK
 						   .xpath("/item/formato[text()='EBOOK']") // Acessando um elemento dentro do arquivo XML, OBS : APOS O SPLIT ACIMA PARA DIVIDIR O ARQUIVO POR ITEM O CAMINHO ATE O FORMATO FICOU MENOR
-						//.log("${id} - ${body}") // Mostrando o ID e o corpo de cada mensagem
-						//.log("${id}") // Mostrando o ID e o corpo de cada mensagem
+						.setProperty("ebookId", xpath("/item/livro/codigo/text()")) // Definindo variavel da rota, clienteId(setando uma propriedade)
 						.marshal().xmljson() // Transformando de XML em JSON
 						.log("${id} - ${body}") // Imprindo a mensagem transformada no console
-						//.setHeader("CamelFileName", constant("pedido.json")) // Alterando o tipo de arquivo de XML para JSON, so que desta forma transformou e enviou apenas 1 arquivo
-						//.setHeader("CamelFileName", simple("${file:name.noext}.json")) // Alterando o tipo de XML para JSON de todos os arquvios
 						.setHeader(Exchange.HTTP_METHOD, HttpMethods.GET) // Faz uma requisicao a um metodo GET no servico do endereço abaixo
-						.setHeader(Exchange.HTTP_QUERY, constant("ebookId=ARQ&pedidoId=2451256&clienteId=edgar.b@abc.com"))// enviando os parametros do metodo GET
-						//.to("file:saida");
+						.setHeader(Exchange.HTTP_QUERY, simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}"))// enviando os parametros do metodo GET
 				        .to("http4://localhost:8080/webservices/ebook/item");
 
 			}
